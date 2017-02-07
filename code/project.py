141f7af45d837b8508548f5a2a8c5343efb1fa54
Hendrikto <hendrik.to@gmail.com>
2017-02-07 17:02:02 +0100

    Load the capture, blur it, then display it.

--- code/project.py
+++ code/project.py
@@ -4,6 +4,15 @@ import numpy as np
 
 # author: Hendrik Werner s4549775
 
+capture_path = "../capture.png"
+
+image = cv2.imread(capture_path)
+imageHSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+imageHSV = cv2.medianBlur(imageHSV, 9)
+
+plt.imshow(cv2.cvtColor(imageHSV, cv2.COLOR_HSV2RGB))
+plt.show()
+
 
 def find_round_object(
         hsv: np.ndarray
