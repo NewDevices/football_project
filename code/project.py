f5acdcc1a22ad50482d1468b38ab61d532f09141
Hendrikto <hendrik.to@gmail.com>
2017-02-13 16:09:44 +0100

    Spin off making a mask into its own function.

--- code/project.py
+++ code/project.py
@@ -14,20 +14,20 @@ plt.imshow(cv2.cvtColor(imageHSV, cv2.COLOR_HSV2RGB))
 plt.show()
 
 
-def find_contours(
+def make_mask(
         hsv: np.ndarray
         , color_lower: list
         , color_upper: list
         , ed_iterations: int = 3
 ):
     """
-    Find the contours of an object.
+    Make a mask that captures pixels in a color range.
 
     :param hsv: The image in HSV format
     :param color_lower: The lower color threshold (in HSV)
     :param color_upper: The upper color threshold (in HSV)
     :param ed_iterations: The number of iterations for erosion and dilation
-    :return: The contours found
+    :return: The generated mask
     """
     color_lower = np.uint8(color_lower)
     color_upper = np.uint8(color_upper)
@@ -36,8 +36,20 @@ def find_contours(
     object_mask = cv2.erode(object_mask, None, iterations=ed_iterations)
     object_mask = cv2.dilate(object_mask, None, iterations=ed_iterations)
 
+    return object_mask
+
+
+def find_contours(
+        mask: np.ndarray
+):
+    """
+    Find the contours in a mask.
+
+    :param hsv: The mask
+    :return: The contours found
+    """
     return cv2.findContours(
-        object_mask
+        mask
         , cv2.RETR_LIST
         , cv2.CHAIN_APPROX_SIMPLE
     )
@@ -59,5 +71,7 @@ def find_round_object(
     return None, None
 
 
-ball_contours = find_contours(imageHSV, [15, 150, 50], [25, 255, 255])[1]
+ball_contours = find_contours(
+    make_mask(imageHSV, [15, 150, 50], [25, 255, 255])
+)[1]
 ball_pos, ball_radius = find_round_object(ball_contours)
