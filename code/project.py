86780add32af65d3397f99011e69429f7d48fada
Hendrikto <hendrik.to@gmail.com>
2017-02-20 17:20:21 +0100

    Only continue searching if some car object is found.

--- code/project.py
+++ code/project.py
@@ -160,5 +160,8 @@ blue_car_contours = find_contours(
     make_mask(imageHSV, [90, 128, 10], [120, 255, 255])
 )[1]
 blue_car_pos, _ = find_round_object(blue_car_contours)
-blue_car_lines = find_angled_lines(blue_car_contours, 32)
-blue_car_tip = find_intersection(*blue_car_lines)
+blue_car_lines = blue_car_tip = None
+
+if blue_car_pos:
+    blue_car_lines = find_angled_lines(blue_car_contours, 32)
+    blue_car_tip = find_intersection(*blue_car_lines)
