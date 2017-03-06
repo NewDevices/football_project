86caa4a89531a1f6eb118fbc66e2cd44453cc4af
Hendrikto <hendrik.to@gmail.com>
2017-03-06 22:21:45 +0100

    Objectify analysis of positional data.
    * Only print positional information for now.

---
+++ code/analyzer.py
@@ -0,0 +1,34 @@
+# author: Hendrik Werner s4549775
+
+from typing import List
+from object_finder import CarFinder, BallFinder
+
+
+class Analyzer(object):
+    def __init__(
+            self,
+            lower_ball: List[List[int]],
+            upper_ball: List[List[int]],
+            lower_blue: List[List[int]],
+            upper_blue: List[List[int]],
+            lower_red: List[List[int]],
+            upper_red: List[List[int]],
+    ):
+        self._ball_finder = BallFinder(lower_ball, upper_ball)
+        self._blue_car_finder = CarFinder(lower_blue, upper_blue)
+        self._red_car_finder = CarFinder(lower_red, upper_red)
+
+    def analyze(
+            self,
+            image_hsv,
+    ) -> None:
+        self._ball_finder.image = image_hsv
+        self._blue_car_finder.image = image_hsv
+        self._red_car_finder.image = image_hsv
+        ball_pos, ball_radius = self._ball_finder.find_ball()
+        blue_car_pos, blue_car_tip = self._blue_car_finder.find_car()
+        red_car_pos, red_car_tip = self._red_car_finder.find_car()
+
+        print("Ball:", ball_pos, ball_radius)
+        print("Blue Car:", blue_car_pos, blue_car_tip)
+        print("Red Car:", red_car_pos, red_car_tip)
--- code/project.py
+++ code/project.py
@@ -1,5 +1,5 @@
 import cv2
-from object_finder import CarFinder, BallFinder
+from analyzer import Analyzer
 
 # author: Hendrik Werner s4549775
 
@@ -9,23 +9,12 @@ image = cv2.imread(capture_path)
 imageHSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 imageHSV = cv2.medianBlur(imageHSV, 3)
 
-finder = BallFinder(
-    [[15, 128, 50]],
-    [[25, 255, 255]],
-    image_hsv=imageHSV,
+analyzer = Analyzer(
+    lower_ball=[[15, 128, 50]],
+    upper_ball=[[25, 255, 255]],
+    lower_blue=[[90, 128, 10]],
+    upper_blue=[[120, 255, 255]],
+    lower_red=[[170, 128, 50], [0, 128, 50]],
+    upper_red=[[180, 255, 255], [10, 255, 255]],
 )
-finder.find_ball()
-
-finder = CarFinder(
-    [[90, 128, 10]],
-    [[120, 255, 255]],
-    image_hsv=imageHSV,
-)
-finder.find_car()
-
-finder = CarFinder(
-    [[170, 128, 50], [0, 128, 50]],
-    [[180, 255, 255], [10, 255, 255]],
-    image_hsv=imageHSV,
-)
-finder.find_car()
+analyzer.analyze(imageHSV)
