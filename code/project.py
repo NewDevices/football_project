e88aca14a3fdf88e227258dfc2793cedda5b4ba7
Hendrikto <hendrik.to@gmail.com>
2017-03-12 17:15:28 +0100

    Load color thresholds from a config file.

--- code/project.py
+++ code/project.py
@@ -1,8 +1,12 @@
 import cv2
+import yaml
 from analyzer import Analyzer
 from planner import Planner
 
 # author: Hendrik Werner s4549775
+
+with open("config.yml") as conf_file:
+    conf = yaml.safe_load(conf_file)
 
 capture_path = "../capture.png"
 
@@ -10,12 +14,12 @@ image = cv2.imread(capture_path)
 imageHSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
 analyzer = Analyzer(
-    lower_ball=[[15, 128, 50]],
-    upper_ball=[[25, 255, 255]],
-    lower_blue=[[90, 128, 10]],
-    upper_blue=[[120, 255, 255]],
-    lower_red=[[170, 128, 50], [0, 128, 50]],
-    upper_red=[[180, 255, 255], [10, 255, 255]],
+    conf["ball"]["lower"],
+    conf["ball"]["upper"],
+    conf["blue_car"]["lower"],
+    conf["blue_car"]["upper"],
+    conf["red_car"]["lower"],
+    conf["red_car"]["upper"],
 )
 planner = Planner(analyzer)
 planner.plan(imageHSV)
